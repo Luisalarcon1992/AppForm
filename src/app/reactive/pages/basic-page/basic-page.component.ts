f202483477dfeaa15b4a4a17cd294f27e78aea63
Luis <luisalarcon1992@gmail.com>
2024-03-12 15:08:25 -0300

    iniciando proyecto

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  template: `
+    <p>
+      basic-page works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class BasicPageComponent {
+
+}
